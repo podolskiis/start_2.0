4731faf9c75ff44e792c3f3f0993d64ec4ef3d82
podolskiis <mail@sergeypodolsky.ru>
2017-07-26 11:57:37 +0300

    Добавлен sass filling-framework

--- gulp-tasks/config.js
+++ gulp-tasks/config.js
@@ -5,64 +5,64 @@ var
 module.exports = {
   path: {
     app: {
-      home:         './app',
+      home:         'app/',
       sass: {
-        src:        './app/sass/main.scss',
-        dest:       './app/css',
-        watch:      './app/sass/**/*.+(scss|sass)',
+        src:        'app/sass/main.scss',
+        dest:       'app/css/',
+        watch:      'app/sass/**/*.+(scss|sass)',
         rename:     'theme.css'
       },
       pug: {
-        src:        './app/pug/*.pug',
-        dest:       './app',
-        watch:      './app/pug/**/*.pug'
+        src:        'app/pug/*.pug',
+        dest:       'app/',
+        watch:      'app/pug/**/*.pug'
       },
       bower: {
-        src:        './app/pug/**/{head,scripts}.pug',
-        dest:       './app/pug',
-        watch:      './bower.json'
+        src:        'app/pug/**/{head,scripts}.pug',
+        dest:       'app/pug/',
+        watch:      'bower.json'
       },
       js: {
-        watch:      './app/js/*.js'
+        watch:      'app/js/*.js'
       }
     }
   },
   dist: {
-    home:           './www',
+    home:           'www/',
     img: {
-      src:          './app/images/**',
-      dest:         './www/images'
+      src:          'app/images/**',
+      dest:         'www/images/'
     },
     useref: {
-      src:          './app/*.html',
-      dest:         './www'
+      src:          'app/*.html',
+      dest:         'www/'
     },
     import: {
       css: {
-        src:        './app/css/*.css',
-        dest:       './www/css'
+        src:        'app/css/*.css',
+        dest:       'www/css/'
   	  },
       fonts: {
-        src:        './app/fonts/**',
-        dest:       './www/fonts'
+        src:        'app/fonts/**',
+        dest:       'www/fonts/'
   	  },
       js: {
-        src:        './app/js/**',
-        dest:       './www/js'
+        src:        'app/js/**',
+        dest:       'www/js'
   	  },
       dop: {
         src: [
-                    './app/*.ico',
-                    './app/.htaccess'
+                    'app/*.ico',
+                    'app/.htaccess'
         ],
-        dest:       './www'
+        dest:       'www/'
   	  }
     },
-    size:           './www/**'
+    size:           'www/**'
   },
   ftp: {
-    home:           './www',
-    url:            './sergeypodolsky.ru/public_html/work/'+project,
+    home:           'www/',
+    url:            'sergeypodolsky.ru/public_html/work/'+project,
     conn: {
       host:         '92.53.96.109',
       user:         login,
@@ -70,19 +70,19 @@ module.exports = {
       parallel:     10
     },
     globs: [
-                    './www/**',
-                    './www/**/.*'
+                    'www/**',
+                    'www/**/.*'
     ]
   },
   ssh: {
     src: [
-                    './www/**',
-                    './www/**/.*'
+                    'www/**',
+                    'www/**/.*'
     ],
     host: {
-      root:         './www',
+      root:         'www/',
       hostname:     login+'@vh54.timeweb.ru',
-      destination:  './sergeypodolsky.ru/public_html/work/'+project,
+      destination:  'sergeypodolsky.ru/public_html/work/'+project,
       archive:      true,
       silent:       false,
       compress:     true
